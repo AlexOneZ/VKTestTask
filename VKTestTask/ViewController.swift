4de4355b0790650076d0982237093a16aa0db4b4
AlexOneZ <kobiale92@gmail.com>
2024-12-01 18:43:56 +0300

    init

---
+++ VKTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKTestTask
+//
+//  Created by Алексей Кобяков on 01.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
